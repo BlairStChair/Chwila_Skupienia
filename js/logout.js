116e2024c6a83b8488d3e653ab232d680caffd69
BlairStChair <julia.z15@interia.pl>
2025-06-08 15:22:51 +0200

    Fixed logout button to prevent going back into the dashboard with browser buttons

--- js/logout.js
+++ js/logout.js
@@ -1,10 +1,16 @@
 const logoutBtn = document.querySelector(".logoutBtn");
 
+auth.onAuthStateChanged((user) => {
+    if (!user) {
+        location.replace("loginPage.html");
+    }
+});
+
 logoutBtn.addEventListener("click", async () => {
     try{
         await auth.signOut();
         alert("Zostałeś wylogowany!");
-        window.location.replace = "loginPage.html";
+        location.replace("loginPage.html");
     }catch (error){
         console.log(error.message);
         alert("Wystąpił błąd podczas wylogowywania!");
