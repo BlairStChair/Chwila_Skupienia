24a98dd574c492250d89861190d53cd378407dac
BlairStChair <julia.z15@interia.pl>
2025-08-10 15:37:32 +0200

    Added todo list script to profile html

---
+++ js/todoList.js
@@ -0,0 +1,7 @@
+let addTask = document.querySelector(".addTask");
+let taskName = document.querySelector(".taskName");
+let taskList = document.querySelector(".taskList");
+
+addTask.addEventListener("click", () => {
+
+});
