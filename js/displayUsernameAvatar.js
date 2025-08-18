4f9d5159e1975be7c07d570496e6b8924ced2347
BlairStChair <julia.z15@interia.pl>
2025-08-18 17:12:19 +0200

    Made script to display username on profile page

--- js/displayUsernameAvatar.js
+++ js/displayUsernameAvatar.js
@@ -1,2 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
 const username = document.querySelector(".username");
 
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+auth.onAuthStateChanged(async (user) => {
+    const name = await db.collection("users").doc(user.uid).get();
+
+    const userName = name.data();
+
+    username.textContent = userName.displayName;
+});
+});
--- js/registerUser.js
+++ js/registerUser.js
@@ -31,7 +31,6 @@ async function writeUsername(user) {
   }
 }
 
-
 registrationForm.addEventListener("submit", async(e) => {
     e.preventDefault();
 
