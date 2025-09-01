667d28c414e5096bba8c881e5658eb03c40ea41e
BlairStChair <julia.z15@interia.pl>
2025-09-01 15:36:13 +0200

    Added Firebase SDK and firebase script

--- js/profileSettings.js
+++ js/profileSettings.js
@@ -1,6 +1,14 @@
 const avatarFile = document.querySelector("#avatarFile");
 const avatar = document.querySelector("#avatar")
+const submitAvatar = document.querySelector("#submitAvatar");
+
+const auth = firebase.auth();
+const db = firebase.firestore();
 
 avatarFile.addEventListener("change", () => {
     avatar.src = URL.createObjectURL(avatarFile.files[0]);
 })
+
+submitAvatar.addEventListener("click", () => {
+
+});
