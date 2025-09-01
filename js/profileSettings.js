1796dce53fbeaa45de1dc62babb64b7b4f37aca1
BlairStChair <julia.z15@interia.pl>
2025-09-01 16:10:27 +0200

    Added firebase storage script

--- js/profileSettings.js
+++ js/profileSettings.js
@@ -7,30 +7,36 @@ const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const database = firebase.database();
 const db = firebase.firestore();
+const storage = firebase.storage();
+const storageRef = storage.ref(); 
 
 avatarFile.addEventListener("change", () => {
-    avatar.src = URL.createObjectURL(avatarFile.file[0]);
+    avatar.src = URL.createObjectURL(avatarFile.files[0]);
 })
 
-submitAvatar.addEventListener("click", async () => {
+submitAvatar.addEventListener("click", async (e) => {
+    e.preventDefault();
+
     try{
-    let isUserLogged = auth.currentUser;
-    if(!isUserLogged){
-        alert("Zaloguj się, aby zmienić zdjęcie profilowe");
-        return;
-    }
+        let isUserLogged = auth.currentUser;
+        if(!isUserLogged){
+            alert("Zaloguj się, aby zmienić zdjęcie profilowe");
+            return;
+        }
 
-    await storageRef.put(file);
-    let avatarURL = await storageRef.getDowloadURL();
+        storageRef = storage.ref().child(`avatars/${user.uid}.jpg`);
+        await storageRef.put(avatarFile.files[0]);
 
-    await db.collection("users").doc(user.uid).update({
-        photoURL: avatarURL
-    });
+        let avatarURL = await storageRef.getDowloadURL();
 
-    alert("Zdjęcie profilowe zostało zmienione pomyślnie!");
-    } catch(err){
-        console.error("Błąd", err);
-        alert("Wystąpił błąd podczas zmiany zdjęcia profilowego!")
-    }
+        await db.collection("users").doc(user.uid).update({
+            photoURL: avatarURL
+        });
+
+        alert("Zdjęcie profilowe zostało zmienione pomyślnie!");
+        } catch(err){
+            console.error("Błąd", err);
+            alert("Wystąpił błąd podczas zmiany zdjęcia profilowego!")
+        }
 });
 });
