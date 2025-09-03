4015bc74217ed9e7a7b5351bf7c7013fb174ec97
BlairStChair <julia.z15@interia.pl>
2025-09-03 13:59:39 +0200

    Search button register input value

--- js/friendsList.js
+++ js/friendsList.js
@@ -1,12 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
 const usersFriends = document.querySelector(".usersFriends");
 const userSearch = document.querySelector(".userSearch");
+const searchResults = document.querySelector(".searchResults")
 
 let userSearchField = document.createElement("input");
 userSearchField.setAttribute("type", "text");
 
+let userSearchBtn = document.createElement("button");
+userSearchBtn.textContent = "Szukaj";
 
 userSearch.addEventListener("click", () => {
     usersFriends.style.display = "none";
     userSearch.appendChild(userSearchField);
+    userSearch.appendChild(userSearchBtn);
     userSearchField.focus();
+});
+
+userSearchBtn.addEventListener("click", () => {
+    let usernameInput = userSearchField.value;
+    console.log(usernameInput);
+});
 });
