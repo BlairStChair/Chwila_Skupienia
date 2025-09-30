7386d37aea3f0ceeece6759a04e24a567cc519bc
BlairStChair <julia.z15@interia.pl>
2025-09-30 13:14:11 +0200

    Added necessary DOM elements and array of added songs

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+const musicPlayer = document.querySelector(".musicPlayer");
+const songTitle = document.querySelector(".songTitle");
+const durationProgress = document.querySelector(".durationProgress");
+const progressBar = document.querySelector(".progressBar");
+const audio = document.querySelector(".audio");
+const previousSong = document.querySelector(".previousSong");
+const playStop = document.querySelector(".playStop");
+const nextSong = document.querySelector(".nextSong");
+
+let songsArray = [
+    "lofi background music",
+    "lofi lofi song",
+    "lofi rain lofi music",
+    "rainy lofi city lofic music"
+]
+
 });
