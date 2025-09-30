fce3d7ee267b9cdb4282e462318f548f551b917f
BlairStChair <julia.z15@interia.pl>
2025-09-30 13:42:19 +0200

    Added functions to change play and stop icons

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
 const musicPlayer = document.querySelector(".musicPlayer");
+const songInfo = document.querySelector(".songInfo");
 const songTitle = document.querySelector(".songTitle");
 const durationProgress = document.querySelector(".durationProgress");
 const progressBar = document.querySelector(".progressBar");
 const audio = document.querySelector(".audio");
 const previousSong = document.querySelector(".previousSong");
 const playStop = document.querySelector(".playStop");
+const playSong = document.querySelector(".playSong");
+const pauseSong = document.querySelector(".pauseSong");
 const nextSong = document.querySelector(".nextSong");
 
 let songsArray = [
@@ -21,9 +24,32 @@ loadSong(songsArray[songIndex]);
 
 function loadSong(song){
     songTitle.textContent = song;
-audio.src = `../assets/songs/${song}.mp3`; 
+    audio.src = `../assets/songs/${song}.mp3`; 
 }
 
+function playSongFunction(){
+    playSong.style.visibility = "hidden";
+    pauseSong.style.visibility = "visible"; 
+}
 
+function pauseSongFunction(){
+    pauseSong.style.visibility = "hidden"; 
+    playSong.style.visibility = "visible";
+}
+
+let clickCount = 1;
+
+playStop.addEventListener("click", () => {
+    
+
+    if(clickCount % 2 !== 0){
+        playSongFunction();
+    }else{
+        pauseSongFunction();
+    }
+
+    clickCount++;
+    console.log(clickCount);
+});
 
 });
