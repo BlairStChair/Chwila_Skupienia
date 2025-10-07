04bd6c6e0987e8611f7264cac9475645bc5c989f
BlairStChair <julia.z15@interia.pl>
2025-10-07 13:12:03 +0200

    Created variables to read required data for password reset

--- js/passwordResetLink.js
+++ js/passwordResetLink.js
@@ -3,6 +3,11 @@ const passwordAfterReset = document.querySelector(".passwordAfterReset");
 const passwordAfterResetConfirm = document.querySelector(".passwordAfterResetConfirm");
 const passwordResetBtn = document.querySelector(".passwordResetBtn");
 
+
+const params = new URLSearchParams(window.location.search);
+const mode = params.get('mode');      
+const oobCode = params.get('oobCode');
+
 passwordResetBtn.addEventListener("click", async(e) => {
     e.preventDefault();
 
@@ -20,7 +25,10 @@ passwordResetBtn.addEventListener("click", async(e) => {
             alert("Nowe hasła nie są takie same!");
             passwordAfterReset.value = "";
             passwordAfterResetConfirm.value = "";
+            return;
         }
+
+
     }catch(err){
             console.error("Błąd", err);
             alert("Wystąpił błąd podczas resetowania hasła!")
