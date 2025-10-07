651a75ee1f39644e9656f7ee627378b5f77d921a
BlairStChair <julia.z15@interia.pl>
2025-10-07 12:50:29 +0200

    Secured password reset from changing password for wrong value

--- js/passwordResetLink.js
+++ js/passwordResetLink.js
@@ -5,5 +5,25 @@ const passwordResetBtn = document.querySelector(".passwordResetBtn");
 
 passwordResetBtn.addEventListener("click", async(e) => {
     e.preventDefault();
+
+    const newPasswordValue = passwordAfterReset.value.trim();
+    const newPasswordConfirmValue = passwordAfterResetConfirm.value.trim();
+
+    try{
+        const user = auth.currentUser;
+        if(!user){
+            alert("Zaloguj się, aby zmienić nazwę użytkownika");
+            return;
+        }
+
+        if(newPasswordValue !== newPasswordConfirmValue){
+            alert("Nowe hasła nie są takie same!");
+            passwordAfterReset.value = "";
+            passwordAfterResetConfirm.value = "";
+        }
+    }catch(err){
+            console.error("Błąd", err);
+            alert("Wystąpił błąd podczas resetowania hasła!")
+    }
 });
 });
