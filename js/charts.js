f81ae431ec997f23c032811b01d5853b37ecfd8c
BlairStChair <julia.z15@interia.pl>
2025-12-04 14:17:42 +0100

    Added function to get dates for previous week

--- js/charts.js
+++ js/charts.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
+function getWeeklyDates(){
+    let todayDate = new Date();
+    let weeklyDates = [];
 
-const weeklyChart = document.querySelector("#weeklyChart");
+    console.log(todayDate);
+
+    for(let i = 7; i > 0; i--){
+        let pastDate = new Date(todayDate.getTime());
+        pastDate.setDate(pastDate.getDate() - i);
+        weeklyDates.push(pastDate.toLocaleDateString("default",
+            {
+                day: "numeric", month: "short"
+            }
+        ));
+    }
+    
+    weeklyDates.push("Today");
+    console.log(weeklyDates);
+    return weeklyDates;
+}
+
+getWeeklyDates();
 
 var xValues = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"];
 var yValues = [10.5, 9, 7.5, 6, 4.5, 3, 1.5];
@@ -9,7 +29,7 @@ var barColors = "#e4d4f8ff";
 new Chart("weeklyChart", {
   type: "bar",
   data: {
-    labels: xValues,
+    labels: getWeeklyDates(),
     datasets: [{
       backgroundColor: barColors,
       data: yValues
