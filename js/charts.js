0486956290cc1fd69684b1217221ff1e8eb2d2e7
BlairStChair <julia.z15@interia.pl>
2025-12-15 14:04:36 +0100

    Cleaned up code

--- js/charts.js
+++ js/charts.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-const auth = firebase.auth();
 const db = firebase.firestore();
 
 let weeklyDatesToGetData = [];
@@ -27,8 +26,6 @@ function getWeeklyDates(){
     monday.setDate(todayDate.getDate() + DaysToMonday);
 
     for(let i = 7; i > 0; i--){
-        // let pastDate = new Date(todayDate.getTime());
-        // pastDate.setDate(pastDate.getDate() - i);
         let date = new Date(monday);
         date.setDate(monday.getDate() - i);
 
@@ -41,36 +38,10 @@ function getWeeklyDates(){
         weeklyDatesToGetData.push(date.toISOString().split("T")[0]);
     }
 
-    // weeklyDates.push("Today");
     console.log(weeklyDatesToDisplay);
     console.log(weeklyDatesToGetData);
     return weeklyDatesToDisplay;
 }
-
-// async function getUserTime(){
-//   return new Promise(resolve => {
-//   auth.onAuthStateChanged(async (user) => {
-//     let uid = user.uid;
-//     let downloadedMinutes = [];
-
-//     for(let i = 0; i < 7; i++){
-//       let docRef = db.collection("sessionsInfo").doc(uid).collection("stats").doc(weeklyDatesToGetData[i]);
-//       let docSnap = await docRef.get();
-
-//       //Wczesniej nie mialam takiego warunku ale musialam go dodac poniewaz dla jednego 
-//       // dnia tygodnia nie mialam zadnego rekordu wiec wywalalo blad
-//       if(docSnap.exists){
-//         downloadedMinutes.push(docSnap.data().minutes | 0);
-//       }else{
-//         downloadedMinutes.push(0);
-//       } 
-//     }
-//     console.log(downloadedMinutes);
-    
-//     resolve(downloadedMinutes);
-//   });
-//   });
-// }
 
 async function getUserStats(uid){
   let weeklyDates = getWeeklyDates();
@@ -92,26 +63,6 @@ async function getUserStats(uid){
   return downloadedMinutes;
 }
 
-// async function createChart(){
-//   const labels = getWeeklyDates();
-//   const data = await getUserTime();
-//   var barColors = "#e4d4f8ff";
-
-//   new Chart("weeklyChart", {
-//     type: "bar",
-//     data: {
-//       labels: labels,
-//       datasets: [{
-//         backgroundColor: barColors,
-//         data: data
-//       }]
-//     },
-//     options: {
-//       legend: {display: false}
-//     }
-//   });
-  
-// }
 async function createChart(statsArray){
   const labels = getWeeklyDates();
   var barColors = "#e4d4f8ff";
@@ -132,7 +83,6 @@ async function createChart(statsArray){
   
 }
 
-// createChart();
 window.getUserStats = getUserStats;
 window.createChart = createChart;
 });
