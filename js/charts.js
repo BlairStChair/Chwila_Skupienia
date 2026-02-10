400d75b3858c7bdd4133795a097b613abe55f911
BlairStChair <julia.z15@interia.pl>
2026-02-10 17:32:48 +0100

    Added comments

--- js/charts.js
+++ js/charts.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 const db = firebase.firestore();
 
+//podaje daty dni z poprzedniego tygodnia 
 function getWeeklyDates(){
+    //pobranie dzisiejszej daty
     let todayDate = new Date();
     let weeklyDatesToDisplay = [];
     let dayOfWeek = todayDate.getDay();
@@ -12,17 +14,20 @@ function getWeeklyDates(){
     console.log(todayDate);
     console.log(dayOfWeek);
     
+    //jeśli mamy niedziele to cofamy się o 6 dni, a jak inny dzień to cofamy się do poniedziałku
     if(dayOfWeek == 0){
       DaysToMonday = 1;
     }else{
       DaysToMonday = 1 - dayOfWeek;
     }
 
+    //ustawiamy datę na poniedziałek bieżącego tygodnia
     monday.setDate(todayDate.getDate() + DaysToMonday);
 
     let displayDates = [];
     let isoDates = [];
 
+    //generuje się 7 dat dni z poprzedniego tygodnia do wyświetlenia na wykresie
     for(let i = 7; i > 0; i--){
         let date = new Date(monday);
         date.setDate(monday.getDate() - i);
@@ -36,20 +41,25 @@ function getWeeklyDates(){
         isoDates.push(date.toISOString().split("T")[0]);
     }
 
+    //zwraca obie tablice z datami z zeszłego tygodnia - jedna dla wykresu, druga w formacie dla kolekcji Firebase
     console.log(weeklyDatesToDisplay);
     return {displayDates, isoDates};
 }
 
+//pobiera statystyki użytkownika z Firestore
 async function getUserStats(uid){
   const { isoDates } = getWeeklyDates()
   let downloadedMinutes = [];
 
+  //iterujemy po każdej dacie z poprzedniego tygodnia i robimy referencje
   for(let date of isoDates){
     let docRef = db.collection("sessionsInfo").doc(uid).collection("stats").doc(date);
+    //pobranie danych z kolekcji na podstawie referencji
     let docSnap = await docRef.get();
 
-    //Wczesniej nie mialam takiego warunku ale musialam go dodac poniewaz dla jednego 
-    // dnia tygodnia nie mialam zadnego rekordu wiec wywalalo blad
+    //wczesniej nie mialam takiego warunku ale musialam go dodac poniewaz dla jednego 
+    //dnia tygodnia nie mialam zadnego rekordu wiec wywalalo blad
+    //chodzi o to, że jeśli dokument z danego dnia istnieje to zapisujemy ile minut jest w rekordzie zapisane a jak nie to wpisujemy, że ktoś pracował 0 minut
     if(docSnap.exists){
       downloadedMinutes.push(docSnap.data().minutes | 0);
     }else{
@@ -57,6 +67,7 @@ async function getUserStats(uid){
     } 
   }
   console.log(downloadedMinutes);
+  //zwraca tablice minut ile minut ktoś pracował danego dnia
   return downloadedMinutes;
 }
   // troche zmieniłam to żeby ten 
@@ -82,7 +93,7 @@ async function createChart(statsArray) {
     options: {
       legend: {
       display: false 
-      },
+      }
     }
   });
 }
