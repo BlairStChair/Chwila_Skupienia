bbb54078f9f7bbcf6ab2e3046441e6a2ca297243
BlairStChair <julia.z15@interia.pl>
2026-02-10 17:53:57 +0100

    Added comments

--- js/logout.js
+++ js/logout.js
@@ -8,6 +8,8 @@ auth.onAuthStateChanged((user) => {
 
 logoutBtn.addEventListener("click", async () => {
     try{
+        //używam funkcji gotowej z firebase, które wylogowuje, żeby nie było, że się nie wyloguje i ktoś ma na zawsze dostęp do aplikacji 
+        //tylko trzeba się logować znowu
         await auth.signOut();
         alert("Zostałeś wylogowany!");
         location.replace("loginPage.html");
--- js/passwordRemind.js
+++ js/passwordRemind.js
@@ -19,6 +19,7 @@ invalidEmail.style.color = "red";
 remindPasswordForm.addEventListener("submit", async(e) => {
     e.preventDefault();
 
+    //jak odświeżysz strone to znika informacja o błędnym emailu
     if(remindPasswordForm.contains(invalidEmailAlert)){
     remindPasswordForm.removeChild(invalidEmailAlert);
     }
@@ -27,20 +28,24 @@ remindPasswordForm.addEventListener("submit", async(e) => {
 
     console.log(emailValue);
 
+    //sprawdza czy email ma poprawny format
     if(!emailPattern.test(emailValue)){
       remindPasswordForm.appendChild(invalidEmailAlert);
       return;
     }else{
         try{
+            //jak tak to ściąga informacje o tym użytkowniku z firebase
             const usersRef = await db.collection("users");
             console.log(usersRef);
             const querySnapshot = await usersRef.where("email", "==", emailValue).get();
             console.log(querySnapshot);
 
+            //jak nie znalazł w firebase użytkownika z takim mailem to wywala, że taki mail nie ma konta
             if(querySnapshot.empty){
                 remindPasswordForm.appendChild(invalidEmail);
                 return;
             }else{
+                //jak znalazł to konto to za pomocą gotowej funkcji z firebase wysyła maila do resetowania hasła
                 await auth.sendPasswordResetEmail(emailValue);
                 console.log("Wysłano email resetujący hasło do:", emailValue);
                 alert("Wysłano wiadomość do resetowania hasła!");
